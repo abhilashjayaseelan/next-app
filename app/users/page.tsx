1f600a5a5d463e56aeb9630d857b676a2f88c917
Abhilash MJ <abhialash.jayaseelan@gmail.com>
2023-09-20 10:49:47 +0530

    daisy ui

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -4,24 +4,40 @@ import React from "react";
 interface User {
   id: string;
   name: string;
+  email: string;
 }
 
 const UsersPage = async () => {
-  const res = await fetch(
-    "https://jsonplaceholder.typicode.com/users",
-    { cache: 'no-store'});
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
   const users: User[] = await res.json();
 
   return (
     <>
-    <h1 className="font-bold">Users</h1>
-      <ul>
-        {users.map(user => <li key={user.id}>{user.name}</li>)}
-      </ul> 
+      <h1 className="font-bold">Users</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
-      <Link href={'/'}><div className="font-bold">{''} ← </div></Link>
+      <Link href={"/"}>
+        <div className="font-bold">{""} ← </div>
+      </Link>
     </>
-  )
+  );
 };
 
 export default UsersPage;
